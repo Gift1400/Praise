34a363f3d86e20f589776d91ab1b1835103780d0
Gift1400 <230221432@mycput.ac.za>
2026-08-20 12:32:59 +0200

    update: new frontend structure

--- backend/src/main/java/za/ac/controller/ChurchSiteController.java
+++ backend/src/main/java/za/ac/controller/ChurchSiteController.java
@@ -1,4 +1,28 @@
 package za.ac.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import za.ac.domain.ChurchSite;
+import za.ac.service.churchSiteService.ChurchSiteServiceImpl;
+
+@RestController
+@RequestMapping("/churchSite")
 public class ChurchSiteController {
+
+    private final ChurchSiteServiceImpl churchSiteService;
+
+    @Autowired
+    public ChurchSiteController(ChurchSiteServiceImpl churchSiteService) {
+        this.churchSiteService = churchSiteService;
+    }
+
+    @PostMapping("/create")
+    public ChurchSite create(@RequestBody ChurchSite churchSite){
+        return churchSiteService.create(churchSite);
+    }
+
+    @GetMapping("/read/{churchSiteId}")
+    public ChurchSite read(@PathVariable String churchSiteId){
+        return churchSiteService.read(churchSiteId);
+    }
 }
